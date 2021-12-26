619824f33ef07fdc04f3f0477aec4b33a569e0ce
Alok Rawat <alokrawatt@gmail.com>
2021-12-26 15:44:34 +0530

    first commit here

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
 import react from "react";
-import reactDom from "react-dom";
-import name, { homeTown, myNames } from "./App";
-// import * as anything from './App';
+import ReactDOM from "react-dom";
+import Cards from "./Cards";
 
-reactDom.render(
+function Card(props) {
+  console.log(props);
+  return (
+    <>
+      <div className="cards">
+        <div className="card">
+          <img src= {props.imgsrc} alt="my_pic" className="card_img" />
+          <div className="card_info">
+            <span className="card_category">{props.title}</span>
+            <h3 className="card_title"> {props.sname} </h3>
+            <a href= {props.link} target="_blank">
+              <button> Watch Now </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(
   <>
-    <ul>
-      <li>Heyssoo..</li>
-      <li> {name} </li>
-      <li> {homeTown} </li>
-      <li> {myNames()} </li>
-
-
-{/* //when adding all functions at once */}
-      {/* <li> {anyname.default} </li>
-      <li> {anyname.homeTown} </li>
-      <li> {anyname.myNames()} </li> */}
-    </ul>
+    <Card
+      imgsrc="https://wallpapercave.com/uwp/uwp374924.jpeg"
+      title="A Netflix Original Series"
+      sname="Dark"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+     imgsrc="https://wallpapercave.com/wp/wp8285287.jpg"
+     title="A Netflix Original Series"
+     sname="Stranger things"
+     link="https://www.netflix.com/in/title/80057281"
+    />
+    <Card
+     imgsrc="https://wallpapercave.com/wp/wp2473076.jpg"
+     title="A Netflix Original Series"
+     sname="Brooklyn Nine Nineark"
+     link="https://www.netflix.com/in/title/70281562"
+    />
+    <Card
+     imgsrc="https://wallpapercave.com/wp/wp5575898.jpg"
+     title="A Netflix Original Series"
+     sname="You"
+     link="https://www.netflix.com/in/title/80211991"
+    />
   </>,
   document.getElementById("root")
 );
