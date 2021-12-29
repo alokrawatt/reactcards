e9780af380ceb4615781cdad1659bcd6a6be7a5b
Alok Rawat <alokrawatt@gmail.com>
2021-12-30 01:54:43 +0530

    best method- final

--- src/index.js
+++ src/index.js
@@ -23,19 +23,19 @@ function Card(props) {
   );
 }
 
-function ncard(val){
-    return ( <Card
-    imgsrc={val.imgsrc}
-    title={val.title}
-    sname={val.sname}
-    link={val.link}
-  />)
-}
-
 ReactDOM.render(
   <>
     <h1 className="header_style">My favorite 5 Netflix Series!</h1>
-    {Sdata.map(ncard)}
+    {Sdata.map((val,index) =>{
+     return ( <Card
+        key = {val.id}
+        imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+      />);
+     },
+    )}
   </>,
   document.getElementById("root")
 );
